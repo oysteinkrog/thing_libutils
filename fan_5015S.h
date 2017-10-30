45d5ffef56de509f8c20e4d37f18901cbf58e388
Øystein Krog <oystein.krog@gmail.com>
2017-10-30 22:22:18 +0100

    fan_5015S: add/move from kmax1

---
+++ fan_5015S.h
@@ -0,0 +1,7 @@
+include <system.scad>
+include <units.scad>
+
+fan_5015S_screwpos = [[20*mm,23*mm],[-18*mm, -20*mm]];
+fan_5015S_Z = 15;
+
+fan_5015S_conn_flowoutput = [[-51*mm/2,51.3*mm/2-20*mm/2,0],-X];
